c5dfb6eeffa55f89c9d3c8351fc07e73c8f7698f
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-03-05 18:32:57 +0100

    update database query

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
+import itertools
 from cassandra.cluster import Cluster
 
 class DB():
 
     @classmethod
     def open_con(cls):
-        cls.cluster = Cluster(['cassandra-c01'], port=9042)
+        cls.cluster = Cluster(['cassandra-c01', 'cassandra-c02'], port=9042)
 
         cls.session = cls.cluster.connect('resto', wait_for_all_pools=True)
         cls.session.execute('USE resto')
@@ -17,13 +18,19 @@ class DB():
     def find_resto_by_id(cls, resto_id):
         cls.open_con()
         data = cls.session.execute(f"SELECT * FROM restaurant WHERE id={resto_id}")
-        dictionaire={}
+        dico = {}
         for element in data:
-            dictionnaire={'id':element[0], 'borough':element[1], 'buildingnum':element[2],
-             'cuisinetype':element[3], 'name':element[4], 'phone':element[5], 'street':element[6], 'zipcode':element[7]}
-
+            dico = {
+                'id':element[0], 
+                'borough':element[1], 
+                'buildingnum':element[2],
+                'cuisinetype':element[3], 
+                'name':element[4], 
+                'phone':element[5], 
+                'street':element[6], 
+                'zipcode':element[7]}
         cls.close_con()
-        return dictionnaire
+        return dico
 
     @classmethod
     def find_resto_by_type(cls, resto_type):
@@ -31,38 +38,43 @@ class DB():
         data = cls.session.execute(f"SELECT * FROM restaurant WHERE cuisinetype='{resto_type}'")
         liste=[]
         for element in data:
-            dictionnaire={'id':element[0], 'borough':element[1], 'buildingnum':element[2],
-             'cuisinetype':element[3], 'name':element[4], 'phone':element[5], 'street':element[6], 'zipcode':element[7]}
+            dictionnaire = {
+                'id':element[0], 
+                'borough':element[1],
+                'buildingnum':element[2],
+                'cuisinetype':element[3], 
+                'name':element[4], 
+                'phone':element[5], 
+                'street':element[6], 
+                'zipcode':element[7]}
             liste.append(dictionnaire)
-        dataReturn={'data':liste}
+        data_return={'data':liste}
         cls.close_con()
-        return dataReturn
+        return data_return
 
     @classmethod
     def find_top_grade(cls, grade):
         cls.open_con()
         data = cls.session.execute(f"SELECT * FROM inspection WHERE grade='{grade}'")
-        listeId=[]
+        liste_id = set()
         for element in data:
-            listeId.append(element[0])
+            liste_id.add(element[0])
 
-        liste=[]
-        for id in listeId[:10]:
-            resto=cls.find_resto_by_id(id)
+        liste = []
+        for i, val in enumerate(itertools.islice(liste_id, 10)):
+            resto = cls.find_resto_by_id(val)
             liste.append(resto)
-        dataReturn={'data':liste}
+        data_return = {'data':liste}
         cls.close_con()
-        return dataReturn
+        return data_return
 
     @classmethod
     def find_nb_inspect(cls, resto_id):
         cls.open_con()
         data = cls.session.execute(f"SELECT * FROM inspection WHERE idrestaurant={resto_id}")
-        liste=[]
+        liste = []
         for element in data:
             liste.append(element)
         cls.close_con()
-        dataReturn={'data':{f"nombre d'inspection du restaurant {resto_id}":len(liste)}}
-        return dataReturn
-
-
+        data_return = {'data':{f"nombre d'inspection du restaurant {resto_id}":len(liste)}}
+        return data_return
