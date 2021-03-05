ba4305ed3c86d2185f343e231c4c29b2fbf9502d
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2021-03-05 17:52:27 +0100

    update api docs

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ from database import DB
 app = FastAPI()
 
 @app.get("/")
-def index():
-    return {"Hello":"World"}
+def index(tags=['Index']):
+    return {"Hello":"For checking the docs go there localhost:88/docs"}
 
 @app.get("/api/resto", tags=["Informations"])
 async def get_resto_id(id=None):
     return DB.find_resto_by_id(id)
 
-@app.get("/api/inspect", tags='Informations')
+@app.get("/api/inspect", tags=['Informations'])
 async def get_nb_inspect(id=None):
     return DB.find_nb_inspect(id)
 
 @app.get("/api/type", tags=['Informations'])
-async def get_resto_cooking_type(typeResto=None):
-    return DB.find_resto_by_type(typeResto)
+async def get_resto_cooking_type(type_resto=None):
+    return DB.find_resto_by_type(type_resto)
 
 @app.get("/api/grade", tags=['TOP 10'])
 async def get_resto_grade(grade=None):
